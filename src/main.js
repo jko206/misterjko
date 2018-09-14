17ba8ce221a83c99822b1c6473ad299fae0372b6
jkeezie <just.jai.ko@gmail.com>
2018-09-14 23:50:20 +0000

    restructured everything so things from /src are bundled into /dist. PHP file doesn't copy, however, so it has to be copied manually.

---
+++ src/main.js
@@ -0,0 +1,19 @@
+
+// Vendor
+import 'jquery';
+import Vue from 'vue';
+
+// UI/UX for each section of homepage
+import './js/sections/landing';
+import './js/sections/my-story';
+import './js/sections/portfolio';
+import './js/sections/profile';
+
+// the tool tip used in resume
+import './js/ui/tool-tip';
+import './js/components/nav';
+
+// styles
+import './sass/main.scss';
+
+// 
---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const extractPlugin = new ExtractTextPlugin({
+  filename: 'main.css',
+});
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015'],
+            }
+          }
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: extractPlugin.extract({
+          use: [
+            'css-loader',
+            'sass-loader',
+          ]
+        })
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+      {
+        test: /\.php?/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+              outputPath: '/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+              outputPath: 'images/',
+            }
+          }
+        ]
+      }
+    ],
+  },
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.js'
+    }
+  },
+  plugins: [
+    extractPlugin,
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      filename: 'index.html',
+    }),
+    // new HtmlWebpackPlugin({
+    //   template: 'src/nav.html',
+    //   filename: 'nav.html',
+    //   chunks: [],
+    // }),
+    new CleanWebpackPlugin([
+      'dist',
+    ]),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      Vue: 'vue',
+    }),
+  ]
+};
